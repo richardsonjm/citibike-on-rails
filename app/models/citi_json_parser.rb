d05ebfee17e12abdd41bd17da6a6aa8936291701
John Richardson <richardsonjm@gmail.com>
2014-02-28 14:38:00 -0500

    remove constant definition that was causing problems with sidekiq/sidetiq

--- app/models/citi_json_parser.rb
+++ app/models/citi_json_parser.rb
@@ -1,18 +1,13 @@
 class CitiJSONParser
-
   CITI_JSON = URI.parse('http://citibikenyc.com/stations/json')
-
-  CURRENT_TIME = Time.at((Time.now.to_f / 5.minutes).round * 5.minutes)
 
   def self.parse
     new.call
   end
 
-
   def current_station_time(seconds = 5.minutes)
     Time.at((Time.now.to_f / seconds).round * seconds)
   end
-
 
   def build_row(number, bikes, free, station_time)
     begin
@@ -30,12 +25,11 @@ class CitiJSONParser
       array.each do |station|
         bikes =  station["availableBikes"]
         free = station["availableDocks"] 
-        station_time = CURRENT_TIME.to_s[0..18].gsub(' ','T')
+        station_time = current_station_time.to_s[0..18].gsub(' ','T')
         number = station["id"]
         build_row(number, bikes, free, station_time)
       end
     rescue
     end
   end
-
 end
